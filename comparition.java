9024f92c515f3d0a3dd7a587e9a0d2e90228a9ea
jithu837 <jithendrasolanki56@gmail.com>
2024-12-03 15:53:09 +0530

    Add files via upload

---
+++ comparition.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class comparition 
+{
+    public static void main(String args[])
+{
+    int a=7;
+    int b=8;
+    System.out.println(a==b);
+    System.out.println(a!=b);
+    System.out.println(a>=b);
+    System.out.println(a<=b);
+    System.out.println(a>b);
+    System.out.println(a<b);
+}
+}
