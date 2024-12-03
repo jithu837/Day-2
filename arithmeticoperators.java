253350ba0ca10b9a68885507edf1382a6a7a3677
jithu837 <jithendrasolanki56@gmail.com>
2024-12-03 10:11:49 +0530

    Add files via upload

---
+++ arithmeticoperators.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class arithmeticoperators
+{
+    public static void main(String args[])
+    {
+        int a=7;
+        int b=8;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
