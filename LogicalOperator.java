b21cde6e233998dbb3db3ef1099e868e0586ec9f
jithu837 <jithendrasolanki56@gmail.com>
2024-12-03 12:02:47 +0530

    Add files via upload

---
+++ LogicalOperator.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class LogicalOperator
+{
+    public static void main(String args[])
+    {
+        System.out.println(true&&true);
+        System.out.println(true||false);
+        System.out.println(!(true));
+    }
+}
